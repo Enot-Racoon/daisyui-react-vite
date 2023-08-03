5776c870a984929021cbcad4801fd25be1cd51ba
Alexey Karasev <enotracoon2020@gmail.com>
2023-08-03 10:18:26 +0700

    Added jest testing

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,5 +1,10 @@
+import { render } from '@testing-library/react'
+
+import App from './App'
+
 describe('Testing', () => {
   it('Test case ...', () => {
+    render(<App />)
     expect(true).toBeTruthy()
   })
 })
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: '0.0.0.0' },
+  server: { host: '0.0.0.0', port: 3000 },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
